d9564e1cec1aefefed8ac49b4534a11c0be5981d
Muirush <rapturesimon@gmail.com>
2022-06-18 09:53:16 +0300

    Update & nav graph

---
+++ app/src/main/java/com/galoppingtech/wafungiconnect/Auth/ValidatePhone.kt
@@ -0,0 +1,14 @@
+package com.galoppingtech.wafungiconnect.Auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.galoppingtech.wafungiconnect.R
+
+
+class ValidatePhone : Fragment(R.layout.fragment_validate_phone) {
+
+
+}
