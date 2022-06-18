2ecc702f5f3cadd139d5eff98e7d89952ce08f29
Muirush <rapturesimon@gmail.com>
2022-06-18 13:09:57 +0300

    Update: Nav, firebase

--- app/src/main/java/com/galoppingtech/wafungiconnect/CreatePost.kt
+++ app/src/main/java/com/galoppingtech/wafungiconnect/CreatePost.kt
@@ -5,7 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.galoppingtech.wafungiconnect.databinding.FragmentCreatePostBinding
+import com.galoppingtech.wafungiconnect.databinding.FragmentHomeBinding
 
 
 class CreatePost : Fragment(R.layout.fragment_create_post) {
+    private var _binding: FragmentCreatePostBinding? = null
+    private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentCreatePostBinding.bind(view)
+
+        binding.bottomNav.setupWithNavController(binding.root.findNavController())
+    }
 }
