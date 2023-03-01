20b499f1c4cb21917ca5bfe6a7182a35aafaa323
Muirush <rapturesymon@gmail.com>
2023-03-01 07:26:50 +0300

    Profile

--- app/src/main/java/com/galoppingtech/wafungiconnect/ProfileFragment.kt
+++ app/src/main/java/com/galoppingtech/wafungiconnect/ProfileFragment.kt
@@ -9,14 +9,32 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.galoppingtech.wafungiconnect.databinding.FragmentInfoBinding
 import com.galoppingtech.wafungiconnect.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentProfileBinding.bind(view)
 
+
         binding.bottomNav.setupWithNavController(binding.root.findNavController())
+
+        auth = FirebaseAuth.getInstance()
+        val  user = auth.currentUser
+        if (auth.currentUser == null){
+            binding.profileName.text = ""
+        }
+        else{
+            binding.profileName.text = user?.email
+        }
+
+
+
+
+
+
     }
 }
